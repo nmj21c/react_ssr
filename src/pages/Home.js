7a04838d75dedfd08030d8921a99d323154caaa4
Jonghyun <nmj21c@gmail.com>
2020-01-18 11:03:15 +0900

    테스트를 위한 가짜 이미지 추가 및 news.scss 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import shibaImg from "../assets/images/shiba.jpg";
 import './Home.scss';
 
 export default () => (
-    <>
-    <div className="home-bg">Home Sweet home3</div>
-    <img src={shibaImg} alt="shiba img" />
-    </>
+    <div className="home">
+        <div className="bg">Home Sweet home3</div>
+        <img src={shibaImg} alt="shiba img" />
+    </div>
 )
--- src/pages/News.js
+++ src/pages/News.js
@@ -3,10 +3,12 @@ import Helmet from "react-helmet";
 import './News.scss';
 
 export default () => (
-    <div className="home-bg">
-        <Helmet>
-            <title>News</title>
-        </Helmet>
-        news
+    <div className="news">
+        <div className="bg">
+            <Helmet>
+                <title>News</title>
+            </Helmet>
+            news
+        </div>
     </div>
 );
